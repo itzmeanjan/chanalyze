f8277b0c899d4aeac2e5e7f2c0411cf9b392e219
Anjan Roy <anjanroy@yandex.com>
2019-10-08 18:48:33 +0530

    hourly contribution to a Chat for each participating users - chart generated

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 from typing import List
 from functools import reduce
 try:
-    from util import plotContributionInChatByUser
+    from util import plotContributionInChatByUser, plotContributionOfUserByHour, shadeContactName
     from model.chat import Chat
 except ImportError as e:
     print('[!]Module Unavailable: {}'.format(str(e)))
@@ -17,7 +17,16 @@ def main() -> float:
                       acc+1 if cur else acc, stat, 0)/len(stat) * 100
 
     try:
-        return __calculatePercentageOfSuccess__([plotContributionInChatByUser(Chat.importFromText('./data/private.txt'), './plots/participationInPrivateChatByUser.png', 'Visualization of Participation of Users in Chat')])
+        return __calculatePercentageOfSuccess__(
+            [  # plotContributionInChatByUser(Chat.importFromText('./data/private.txt'), './plots/participationInPrivateChatByUser.png', 'Visualization of Participation of Users in Chat'),
+                # plotContributionInChatByUser(Chat.importFromText(
+                #  './data/group.txt'), './plots/participationInGroupChatByUser.png', 'Visualization of Participation of Users in Chat'),
+                *reduce(lambda acc, cur:
+                        [plotContributionOfUserByHour(
+                            cur.messages, './plots/contributionInPrivateChatOf{}ByHour.png'.format(
+                                shadeContactName('_'.join(cur.name.split(' ')))),
+                            'Visualization of {}\'s Participation in Private Chat'.format(shadeContactName(cur.name)))] + acc,
+                        Chat.importFromText('./data/private.txt').users, [])])
     except Exception:
         return 0.0
 
--- util.py
+++ util.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 
+from __future__ import annotations
 from os.path import abspath, dirname, exists
 from os import mkdir
+from functools import reduce
+from typing import Dict, List
+from collections import OrderedDict
 try:
     from matplotlib import pyplot as plt
     from matplotlib.ticker import MultipleLocator, PercentFormatter
     from model.chat import Chat
+    from model.user import User
+    from model.message import Message
+    from model.timePeriod import TimePeriod
 except ImportError as e:
     print('[!]Module Unavailable : {}'.format(str(e)))
     exit(1)
@@ -22,6 +29,12 @@ def directoryBuilder(targetPath: str):
 # shades last half of characters of Contact by `*`, for sake of privacy
 def shadeContactName(name: str) -> str:
     return name[:-(len(name)//2)]+'*'*(len(name)//2)
+
+
+'''
+    How a certain person contributed to a Chat between two persons ( Private )
+    or more ( Group ), is plotted into a Bar Chart ( shows contribution in percentage )
+'''
 
 
 def plotContributionInChatByUser(chat: Chat, targetPath: str, title: str) -> bool:
@@ -55,6 +68,51 @@ def plotContributionInChatByUser(chat: Chat, targetPath: str, title: str) -> boo
             plt.tight_layout()
             plt.savefig(targetPath, bbox_inches='tight', pad_inches=.5)
             plt.close()
+        return True
+    except Exception:
+        return False
+
+
+def plotContributionOfUserByHour(messages: List[Message], targetPath: str, title: str) -> bool:
+
+    def __buildStatusHolder__(holder: Dict[str, int], current: Message) -> Dict[str, int]:
+        tm = current.timeStamp.timetz()
+        seconds = tm.hour*3600 + tm.minute*60
+        found = str(
+            reduce(lambda acc, cur: cur if seconds in cur else acc, ranges, None))
+        holder.update({found: holder.get(found, 0)+1})
+        return holder
+
+    try:
+        directoryBuilder(targetPath)
+        ranges = [TimePeriod(i, i+3600*3, 1)
+                  for i in range(0, 86400, 3600*3)]
+        splitted = reduce(__buildStatusHolder__, messages,
+                          dict([(str(i), 0) for i in ranges]))
+        _tmpLabels = sorted(splitted)
+        data = [splitted[i] for i in _tmpLabels]
+        splitted = OrderedDict([(i, splitted[i])
+                                for i in _tmpLabels])
+        _tmpLabels = [i for i in splitted]
+        data = [splitted[i] for i in _tmpLabels]
+        total = sum(data)
+        labels = ['{} ( {:7.4f} % )'.format(i, splitted[i]*100/total)
+                  for i in _tmpLabels]
+        font = {
+            'family': 'serif',
+            'color': '#000000',
+            'weight': 'normal',
+            'size': 16
+        }
+        plt.figure(figsize=(24, 12), dpi=100)
+        patches, _ = plt.pie(data)  # plotting pie chart
+        plt.legend(patches, labels, loc='best', fontsize='medium')
+        plt.title(title, fontdict=font)
+        plt.axis('equal')
+        plt.tight_layout()
+        plt.savefig(targetPath, bbox_inches='tight',
+                    pad_inches=.5)  # exporting plotted PIE chart
+        plt.close()  # closing this figure on which we just plotted a PIE chart
         return True
     except Exception:
         return False
