fd406d407e32af29bacdc48c50da3e9b33ae68e5
Anjan Roy <anjanroy@yandex.com>
2019-10-17 23:29:26 +0530

    extracting emojis used in a chat

---
+++ emoji.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+from __future__ import annotations
+from functools import reduce
+from unicodedata import name as getNameOfUnicodeChar
+try:
+    from model.chat import Chat
+except Exception as e:
+    print('[!]Module Unavialable : {}'.format(str(e)))
+    exit(1)
+
+
+def findUnicodeCharacters(chat: Chat):
+    return map(getNameOfUnicodeChar,
+               reduce(lambda acc, cur:
+                      acc +
+                      reduce(lambda accInner, curInner:
+                             accInner +
+                             reduce(lambda accInnerDeep, curInnerDeep:
+                                    (accInnerDeep + [curInnerDeep]) if ord(
+                                        curInnerDeep) > 255 else accInnerDeep,
+                                    curInner.content, []),
+                             cur.messages, []),
+                      chat.users, []))
+
+
+if __name__ == '__main__':
+    print('[!]This module is designed to be used as a backend handler')
+    exit(0)
