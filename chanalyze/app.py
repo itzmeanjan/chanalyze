aa2c70cbb12575012bc7543e196944799ea79615
Anjan Roy <anjanroy@yandex.com>
2020-06-19 21:53:21 +0530

    Ready to push v0.3.4, added capability to fetch emoji data from disk ( if available )

--- chanalyze/app.py
+++ chanalyze/app.py
@@ -77,7 +77,7 @@ def main():
 
     # prints usage of this script
     def _banner():
-        print('\x1b[1;6;36;49m[+]chanalyze v0.3.3 - A simple WhatsApp Chat Analyzer\x1b[0m\n\n\t\x1b[3;30;47m$ chanalyze `path-to-exported-chat-file` `path-to-sink-directory`\x1b[0m\n\n[+]Author: Anjan Roy<anjanroy@yandex.com>\n[+]Source: https://github.com/itzmeanjan/chanalyze ( MIT Licensed )\n')
+        print('\x1b[1;6;36;49m[+]chanalyze v0.3.4 - A simple WhatsApp Chat Analyzer\x1b[0m\n\n\t\x1b[3;30;47m$ chanalyze `path-to-exported-chat-file` `path-to-sink-directory`\x1b[0m\n\n[+]Author: Anjan Roy<anjanroy@yandex.com>\n[+]Source: https://github.com/itzmeanjan/chanalyze ( MIT Licensed )\n')
 
     startTime = time()
     endTime = startTime
@@ -99,6 +99,9 @@ def main():
         # this instance will live throughout lifetime of this script
         chat = Chat.importFromText(sourceFile)
         emojiData = importFromFile() or exportToFile()
+
+        if not emojiData:
+            raise Exception('Unable to fetch emoji data')
 
         extension = makeOutputChoice()
         if not extension:
