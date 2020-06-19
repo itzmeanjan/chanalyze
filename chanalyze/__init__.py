1845f65d023c4c093e682e138d199000b9b8b542
Anjan Roy <anjanroy@yandex.com>
2020-06-19 08:59:06 +0530

    Finally ready to push v0.3.3

--- chanalyze/__init__.py
+++ chanalyze/__init__.py
@@ -1,5 +1,11 @@
 '''
 A simple WhatsApp Chat Analyzer ( for both Private & Group chats ), made with <3
+
+- Chanalyze aims to be a easy tool, for analyzing your Whatsapp Chat(s).
+- No data sent to remote, everything stays in your machine.
+
+[+]Author: Anjan Roy<anjanroy@yandex.com>
+[+]Source: https://github.com/itzmeanjan/chanalyze ( MIT Licensed )
 '''
 
-__version__ = '0.3.2'
+__version__ = '0.3.3'
